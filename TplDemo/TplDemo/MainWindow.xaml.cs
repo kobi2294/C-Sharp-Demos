e09bd4d53bb755147b6cdb096ec44a62ba84e1f0
Kobi Hari <kobi2294@yahoo.com>
2019-07-06 02:42:08 +0300

    After async await sample

---
+++ TplDemo/TplDemo/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TplDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_old(object sender, RoutedEventArgs e)
+        {
+            progressBar.IsIndeterminate = true;
+            statusText.Text = "Calculating, please wait...";
+
+            var task = Task.Factory.StartNew(() =>
+            {
+                var results = PrimesCalculator.GetAllPrimes(1000, 180000);
+                return results;
+
+            });
+
+            task.ContinueWith(t =>
+            {
+                resultsList.ItemsSource = task.Result;
+
+                progressBar.IsIndeterminate = false;
+                statusText.Text = "Calculation Completed";
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                progressBar.IsIndeterminate = true;
+                statusText.Text = "Calculating, please wait...";
+
+                resultsList.ItemsSource = await Task.Factory.StartNew(() => PrimesCalculator.GetAllPrimes(1000, 180000));
+                statusText.Text = "Calculation Completed";
+            }
+            catch (Exception)
+            {
+                statusText.Text = "Calculation Failed";
+            }
+            finally {
+                progressBar.IsIndeterminate = false;
+            }
+        }
+
+
+    }
+}
---
+++ TplDemo/TplDemo/PrimesCalculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TplDemo
+{
+    public static class PrimesCalculator
+    {
+        private static bool _isPrime(int number)
+        {
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0) return false;
+            }
+
+            return true;
+        }
+
+        private static IEnumerable<int> _allPrimes(int startNumber, int endNumber)
+        {
+            for (int i = startNumber; i < endNumber; i++)
+            {
+                if (_isPrime(i))
+                    yield return i;
+            }
+        }
+
+        public static List<int> GetAllPrimes(int startNumber, int endNumber)
+        {
+            return _allPrimes(startNumber, endNumber).ToList();
+        }
+    }
+}
